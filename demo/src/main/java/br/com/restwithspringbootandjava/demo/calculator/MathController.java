d184fe30557208072b82bcc919eff822ee5f0acd
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-10-31 19:48:18 -0300

    adicionada implementacao dos metodos para a soma

--- demo/src/main/java/br/com/restwithspringbootandjava/demo/calculator/MathController.java
+++ demo/src/main/java/br/com/restwithspringbootandjava/demo/calculator/MathController.java
@@ -13,17 +13,22 @@ public class MathController {
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-        if(!isNumber(numberOne) || !isNumber(numberTwo)){
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
             throw new Exception();
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String numberOne) {
-        return null;
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",",".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
     }
 
-    private boolean isNumber(String numberOne) {
-        return false;
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
