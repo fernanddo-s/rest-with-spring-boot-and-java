edf630924ec8cb78586c4c0e7796d67d5a3635cc
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-11-01 19:41:40 -0300

    adicionado exceptions customizadas

--- demo/src/main/java/br/com/restwithspringbootandjava/demo/calculator/MathController.java
+++ demo/src/main/java/br/com/restwithspringbootandjava/demo/calculator/MathController.java
@@ -1,5 +1,6 @@
 package br.com.restwithspringbootandjava.demo.calculator;
 
+import br.com.restwithspringbootandjava.demo.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,7 @@ public class MathController {
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
         if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value!");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
