2ba4ec051e776dcd09dcd3ac0a04132897fcc78c
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-11-16 20:45:15 -0300

    adicionado copia com versionamento de api

---
+++ rest-api-versionada-spring-boot-java/src/main/java/com/example/project/controllers/PersonController.java
@@ -0,0 +1,50 @@
+package com.example.project.controllers;
+
+import com.example.project.data.dto.v1.PersonDTO;
+import com.example.project.data.dto.v2.PersonDTOV2;
+import com.example.project.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonDTO> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findById(@PathVariable(value = "id") Long id){
+        return service.findById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO create(@RequestBody PersonDTO person){
+        return service.create(person);
+    }
+
+    @PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTOV2 createV2(@RequestBody PersonDTOV2 person){
+        return service.createV2(person);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO update(@RequestBody PersonDTO person){
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ rest-api-versionada-spring-boot-java/src/main/java/com/example/project/service/PersonService.java
@@ -0,0 +1,78 @@
+package com.example.project.service;
+
+import com.example.project.data.dto.v1.PersonDTO;
+import com.example.project.data.dto.v2.PersonDTOV2;
+import com.example.project.exceptions.ResourceNotFoundException;
+import com.example.project.mapper.ObjectsMapper;
+import com.example.project.mapper.custom.PersonMapper;
+import com.example.project.model.Person;
+import com.example.project.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonService {
+
+    @Autowired
+    PersonRepository repository;
+
+    @Autowired
+    PersonMapper mapper;
+
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public PersonDTO findById(Long id){
+        logger.info("Finding one person!");
+
+        PersonDTO p = new PersonDTO();
+        p.setFirstName("Fernando");
+        p.setLastName("Sousa");
+        p.setAddress("Quixadá - Ceará - Brasil");
+        p.setGender("Male");
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records for this ID!"));
+        return ObjectsMapper.parseObject(entity, PersonDTO.class);
+    }
+
+    public List<PersonDTO> findAll(){
+        logger.info("Finding all people!");
+        return ObjectsMapper.parseListObjects(repository.findAll(), PersonDTO.class);
+    }
+
+    public PersonDTO create(PersonDTO person){
+        logger.info("Creating one person!");
+        var entity = ObjectsMapper.parseObject(person, Person.class);
+        var dto = ObjectsMapper.parseObject(repository.save(entity), PersonDTO.class);
+        return dto;
+    }
+
+    public PersonDTOV2 createV2(PersonDTOV2 person){
+        logger.info("Creating one person with V2!");
+        var entity = mapper.convertDTOToEntity(person);
+        var dto = mapper.convertEntityToDTO(repository.save(entity));
+        return dto;
+    }
+
+    public PersonDTO update(PersonDTO person){
+        logger.info("Updating one person!");
+        var entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        var dto = ObjectsMapper.parseObject(repository.save(entity), PersonDTO.class);
+        return dto;
+    }
+
+    public void delete(Long id){
+        logger.info("Deleting one person!");
+        var person = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records for this ID!"));
+        repository.delete(person);
+    }
+
+}
