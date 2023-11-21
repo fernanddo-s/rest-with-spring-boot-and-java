92c83752fd5818c8ca92b4edfefa68ad5eb849e2
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-11-20 21:04:10 -0300

    adiconado teste para o findById com mockito

---
+++ rest-api-spring-boot-java/src/test/java/com/example/project/unittests/mockito/services/PersonServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.project.unittests.mockito.services;
+
+import com.example.project.model.Person;
+import com.example.project.repositories.PersonRepository;
+import com.example.project.service.PersonService;
+import com.example.project.unittests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonService service;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUpMocks() throws Exception{
+        input = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindById(){
+        Person person = input.mockEntity(1);
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getId());
+        assertNotNull(result.getLinks());
+        System.out.println(result.toString());
+        assertTrue(result.toString().contains("links: [</person/1>;rel=\"self\"]"));
+        assertEquals("Address Test1",result.getAddress());
+        assertEquals("First Name Test1",result.getFirstName());
+        assertEquals("Last Name Test1",result.getLastName());
+        assertEquals("Female",result.getGender());
+    }
+
+}
