8fbb6ee1508274a7e2a1f64fb003ba36cbce68ca
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-11-22 20:06:50 -0300

    correção na importação da nova exception

--- rest-api-spring-boot-java/src/main/java/com/example/project/exceptions/RequiredObjectIsNullException.java
+++ rest-api-spring-boot-java/src/main/java/com/example/project/exceptions/RequiredObjectIsNullException.java
@@ -1,4 +1,4 @@
-package com.example.project.exceptions.handler;
+package com.example.project.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
