d817151183e0b7dae38b9acf64bd338b81ccbd10
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-11-25 16:44:03 -0300

    adicioando documentação da api

--- rest-api-spring-boot-java/src/main/java/com/example/project/controllers/PersonController.java
+++ rest-api-spring-boot-java/src/main/java/com/example/project/controllers/PersonController.java
@@ -87,6 +87,16 @@ public class PersonController {
     }
 
     @DeleteMapping(value = "/{id}")
+    @Operation(summary = "Deletes a Person", description = "Deletes a Person",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204",content = @Content),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<?> delete(@PathVariable(name = "id") Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
