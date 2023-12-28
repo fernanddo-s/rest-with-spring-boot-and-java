81ea15bba90a0bb4ba34d8fe2af99c9d16fb486c
Luis Fernando <fernando.osousa@alu.ufc.br>
2023-12-28 15:06:59 -0300

    adicioanda classe de configuração inicial dos testes de integração

---
+++ rest-api-spring-boot-java/src/test/java/com/example/project/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.example.project.integrationtests.testcontainers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+    public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        static PostgreSQLContainer<?> postgre = new PostgreSQLContainer<>("postgresql:16.1");
+        private static void startContaniners(){
+            Startables.deepStart(Stream.of(postgre)).join();
+        }
+
+        private static Map<String, String> createConnectionConfiguration() {
+            return Map.of(
+                    "spring.datasource.url", postgre.getJdbcUrl(),
+                    "spring.datasource.username", postgre.getUsername(),
+                    "spring.datasource.password", postgre.getPassword()
+            );
+        }
+
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContaniners();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource("testcontaniners", (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+
+
+    }
+}
